2f65dfb09c1b8eeedaaa50d0f9f2a69cfa9da3ae
Sergey Alirzaev <zl29ah@gmail.com>
2019-09-11 02:44:36 +0300

    + LCD backlight PWM frequency patch

--- menu.sh
+++ menu.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -x
 dialog --checklist 'Choose the desired patches' 0 0 0 \
-	fn-swap		'Swap Fn and Ctrl keys'					off \
-	lcd-brightness	'Allow lowering brightness to 1%'			on \
-	fan-72deg	'Make fan blow at 100% speed on 72+°C CPU temperature'	on \
+	fn-swap			'Swap Fn and Ctrl keys'					off \
+	lcd-brightness		'Allow lowering LCD backlight brightness to 1%'		on \
+	lcd-backlight-925hz	'Increase the LCD backlight PWM frequency to ~925Hz'	on \
+	fan-72deg		'Make fan blow at 100% speed on 72+°C CPU temperature'	on \
 	2> selected
 for p in $(cat selected); do
 	fn="patches/$p.rapatch"
