6ccbb58d836ce6074d110bfb52ba6fa8a762e435
Sergey Alirzaev <zl29ah@gmail.com>
2019-09-10 04:05:22 +0300

    + lcd brightness patch

--- menu.sh
+++ menu.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-dialog --checklist 'Choose the desired patches' 0 0 0 fn-swap 'Swap Fn and Ctrl keys' off 2> selected
+set -x
+dialog --checklist 'Choose the desired patches' 0 0 0 \
+	fn-swap		'Swap Fn and Ctrl keys'				off \
+	lcd-brightness	'Allow lowering brightness to 1%'		on \
+	2> selected
 for p in $(cat selected); do
 	fn="patches/$p.rapatch"
-	r2 -w -q -P "$fn" ec.bin
+	r2 -w -q -P "$fn" ec.bin || true	# see https://github.com/radare/radare2/issues/15002
 done
