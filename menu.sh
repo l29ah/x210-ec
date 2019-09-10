939d04f447c0f1d9a4611c94e975df88a62fff15
Sergey Alirzaev <zl29ah@gmail.com>
2019-09-11 02:57:40 +0300

    verify the patches exist before trying to apply them
    since radare2 sucks

--- menu.sh
+++ menu.sh
@@ -8,5 +8,9 @@ dialog --checklist 'Choose the desired patches' 0 0 0 \
 	2> selected
 for p in $(cat selected); do
 	fn="patches/$p.rapatch"
+	if [ ! -e "$fn" ]; then
+		echo "The patch \"$fn\" doesn't exist!"
+		exit 1
+	fi
 	r2 -w -q -P "$fn" ec.bin || true	# see https://github.com/radare/radare2/issues/15002
 done
