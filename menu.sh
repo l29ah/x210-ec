59a88277c1fedf48d304232ba89630593c06e23e
Joshua Wise <joshua@joshuawise.com>
2020-08-29 13:56:14 -0700

    Add vladisslav2001 battery bugfixes.

--- menu.sh
+++ menu.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -x
 dialog --checklist 'Choose the desired patches' 0 0 0 \
-	ec-hotpatch		'Enable EC hotpatching'					off \
 	fn-swap			'Swap Fn and Ctrl keys'					off \
 	lcd-brightness		'Allow lowering LCD backlight brightness to 1%'		on \
 	lcd-backlight-925hz	'Increase the LCD backlight PWM frequency to ~925Hz'	on \
 	fan-72deg		'Make fan blow at 100% speed on 72+°C CPU temperature'	on \
 	enable-hotkeys		'Generate scancodes for all hotkeys'			on \
+	ec-hotpatch		'Enable EC hotpatching'					off \
+	battery-bugfixes	'Vladisslav2001 battery bugfixes'			off \
 	2> selected
 for p in $(cat selected); do
 	fn="patches/$p.rapatch"
